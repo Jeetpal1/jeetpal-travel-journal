10b2a22816c37fd29164b5d874b095fbdb83d919
Jeetpal <singhjeetpal99@gmail.com>
2022-05-02 18:45:15 -0500

    Completed the project, making it dynamic

---
+++ src/components/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Journal from './Journal';
+import data from '../data'
+
+function Content() {
+  return (
+    <div className='content'>
+        {data.map(journal => {
+            return (
+            <Journal 
+                key = {journal.title}
+                {...journal}
+            />)
+
+        })}
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1 className='header--title'><img src='./images/globe.png' alt='globe logo' className='header--globe'/>Jeetpal's Travel Journal</h1>
+    </div>
+  )
+}
+
+export default Header
